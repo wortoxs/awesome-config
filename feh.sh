7a70c331276519c2314f6ccf0d712b286bae6955
denstiny <22545228017@qq.com>
2020-05-19 13:02:56 +0800

    更改切换窗口快捷键

--- feh.sh
+++ feh.sh
@@ -1,6 +1,4 @@
 #!bin/bash
-exec picom &
-exec xmodmap .xmodmaprc
 while true
 do 
 	feh --randomize --bg-fill ~/imager
--- rc.lua
+++ rc.lua
@@ -263,13 +263,13 @@ globalkeys = gears.table.join(
               {description = "show main menu", group = "awesome"}),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
+    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx(  1)    end,
               {description = "swap with next client by index", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
+    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx( -1)    end,
               {description = "swap with previous client by index", group = "client"}),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
+    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative( 1) end,
               {description = "focus the next screen", group = "screen"}),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
+    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative(-1) end,
               {description = "focus the previous screen", group = "screen"}),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
@@ -580,8 +580,9 @@ autorunApps =
 { 
 	"killall bash ",
     --"fcitx",
+	"picom",
 	"flameshot",
-	"exec bash sleep 10 &",
+	"exec bash sleep 5 &",
  	"bash /home/chroot/.config/awesome/feh.sh",
 }
 
