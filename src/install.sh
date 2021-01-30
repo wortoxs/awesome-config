08404e1e27a9ad7562ce1926f452e6501c17d67a
denstiny <2254228017@qq.com>
2021-01-30 15:55:49 +0800

    调整安装程序

--- src/install.sh
+++ src/install.sh
@@ -1,5 +1,8 @@
 #bin/shell
 ls /bin/ | grep yay &> /dev/null
+if [$? == 1 ]
+	sudo pacman -S yay
+ls /bin/ | grep yay &> /dev/null
 if [ $? == 0 ];
 then
 	echo "yay install"
@@ -25,4 +28,16 @@ else
 	echo "picom config"
 	mv ~/.config/awesome/src/picom.conf ~/.config/picom.conf
 fi
-
+ls ~/.config/ grep sxhkd &> /dev/null
+if [ $? == 0 ];
+then
+	echo "config sxhkd"
+	mv ~/.config/sxhkd ~/.config/sxhkd.mk
+	mv ~/.config/awesome/sxhkd ~/.config/
+else
+	mv ~/.config/awesome/sxhkd ~/.config/
+fi
+echo "克隆浮动窗口管理工具"
+git clone https://github.com/Elv13/collision.git ~/.config/awesome/collision
+echo "安装完成,你可以使用Win+s显示快捷键的帮助文档哦"
+echo "感谢支持"
