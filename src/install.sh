f6f218a0f039fabf195dae340b344a8686c30b5f
denstiny <2254228017@qq.com>
2021-02-08 18:19:25 +0800

    shell

--- src/install.sh
+++ src/install.sh
@@ -10,6 +10,9 @@ if [ $? == 0 ];
 then
 	echo "yay install"
 	yay -S xfce4-panel picom-jonaburg-git feh awesome sxhkd alacritty
+else
+	 yay 安装失败，请手动安装yay后重新启动脚本
+	 exit
 fi
 ls /bin/ | grep picom
 if [ $? == 1 ];then
@@ -38,5 +41,8 @@ echo "克隆浮动窗口管理工具"
 cd $HOME && git clone https://hub.fastgit.org/Elv13/collision.git ~/.config/awesome/collision
 echo "安装完成,你可以使用Win+s显示快捷键的帮助文档哦"
 echo "感谢支持"
-notify-send -i ~/.config/awesome/src/s.jpg -t 0 如果你很喜欢 是否可以请我
-喝一杯奶茶
+notify-send -i ~/.config/awesome/src/s.jpg -t 0 如果你很喜欢 是否可以请我喝一杯奶茶
+if [ $? = 0 ];
+then 
+	 echo "抱歉安装失败，你可以向我反馈问题"
+fi
